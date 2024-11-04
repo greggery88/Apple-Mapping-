1ac00ffb27ff00516c2ec04c3989eb59d20b33fe
jwilliamson <medbwi@gmail.com>
2024-11-04 12:53:46 -0500

    added some imports and main functions

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import numpy
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+
+def main():
+    print("hello world")
+    raw_data = pd.read_excel()
+
+if __name__ == '__main__':
+    main()
