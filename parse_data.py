7db11bdd5f2b9e6e5af0aa2b7bc20ba99518f4b1
jwilliamson <medbwi@gmail.com>
2025-02-07 11:04:12 -0500

    update a lot

--- parse_data.py
+++ parse_data.py
@@ -4,10 +4,24 @@ import os
 import geoplot as gplt
 import geopandas as geopd
 import geoplot.crs as gcrs
+import time
+
+from pandas.conftest import datetime64_dtype
 
 
-def get_date_from_excel():
+def _cleanup(label):
+    return (
+        label.lower()
+        .strip()
+        .replace("?", "")
+        .replace("()", "")
+        .replace("  ", " ")
+        .replace("(pears)", "(pear)")
+    )
 
+
+def get_date_from_county_fairs():
+    timetotal_start = time.time()
     rows = []
 
     file_list = os.listdir(
@@ -15,8 +29,8 @@ def get_date_from_excel():
     )
     t = 0
     for county in file_list:
+        time_start = time.time()
         t += 1
-        print(f"{t}/{len(file_list)}")
 
         sheet_names = pd.ExcelFile(f"Maine Fairs/{county}").sheet_names
         sheet_names.remove("Varieties List")
@@ -25,71 +39,186 @@ def get_date_from_excel():
             " County Fairs.xlsx", ""
         )
 
+        print(f"{t}/{len(file_list)}, {county_name}")
+
         for fair in sheet_names:
-            df = pd.read_excel(f"Maine Fairs/{county}", sheet_name=fair)
+            df = pd.read_excel(
+                f"Maine Fairs/{county}", sheet_name=fair, keep_default_na=False
+            )
 
             years = []
             for c in df.columns.values:
                 if str(c).isdigit():
                     years.append(c)
+            try:
+                for year in years:
+                    for i in range(df.shape[0]):
+                        if not pd.isnull(df[year].iloc[i]):
 
-            for year in years:
-                for i in range(df.shape[0]):
-                    if not pd.isnull(df[year].iloc[i]):
-                        apple = df["Variety"].iloc[i]
+                            given_id: str = df["Variety"].iloc[i]
+                            alt_id: str = df["Alt. Name Given"].iloc[i]
+                            presumed_id: str = df["Presumed ID"].iloc[i]
 
-                        rows.append(
-                            dict(
-                                county=county_name,
-                                fair=fair,
-                                year=year,
-                                apple_type=apple,
+                            uncertain = "?" in presumed_id
+
+                            is_apple = True
+
+                            given_id_clean = _cleanup(given_id)
+
+                            # given_id = given_id
+                            if "(pear)" in given_id_clean:
+                                is_apple = False
+                                given_id_clean = given_id_clean.replace(
+                                    "(pear)", ""
+                                ).strip()
+
+                            presumed_id_clean = _cleanup(presumed_id)
+                            if "(pear)" in presumed_id_clean:
+                                presumed_id_clean = presumed_id_clean.replace(
+                                    "(pear)", ""
+                                ).strip()
+                                is_apple = False
+
+                            alt_id_clean = _cleanup(alt_id)
+
+                            rows.append(
+                                dict(
+                                    County=county_name,
+                                    Fair=fair,
+                                    Year=year,
+                                    IsApple=is_apple,
+                                    Given_ID=given_id,
+                                    Given_ID_Clean=given_id_clean,
+                                    Alt_ID=alt_id,
+                                    Alt_ID_Clean=alt_id_clean,
+                                    Presumed_ID=presumed_id,
+                                    Presumed_ID_Clean=presumed_id_clean,
+                                    Uncertain=uncertain,
+                                )
                             )
-                        )
+            except Exception as e:
+                g = list(df.columns)
+                if g[2] != "Presumed ID":
+                    print(
+                        f"ERROR: Presumed ID is wrong it is ({g[2]}), the error is in {county} {fair}"
+                    )
+                if g[1] != "Alt. Name Given":
+                    print(
+                        f"ERROR: Alt. Name Given is wrong it is ({g[1]}), the error is in {county} {fair}"
+                    )
+                print(e)
+                print("ERROR: unknown")
 
+        print(f" time: {time.time() - time_start}s")
+    print(f" time total: {time.time() - timetotal_start}s")
     data = pd.DataFrame.from_records(rows)
+    print("done reading files.")
     return data
 
 
+def get_data_from_source():
+    rows = []
+
+    file_list = os.listdir(
+        "/Users/jwilliamson/git/plasma_particle_simulator/Apple-mapping/fair sources"
+    )
+    for county in file_list:
+        sheet_names = pd.ExcelFile(
+            f"/Users/jwilliamson/git/plasma_particle_simulator/Apple-mapping/fair sources/{county}"
+        ).sheet_names
+
+        county_name = county.replace("Copy of Maine, ", "").replace(
+            " County Sources.xlsx", ""
+        )
+        for fair in sheet_names:
+            sd = pd.read_excel(
+                f"/Users/jwilliamson/git/plasma_particle_simulator/Apple-mapping/fair sources/{county}",
+                sheet_name=fair,
+                keep_default_na=False,
+            )
+
+            s = sd.shape[0]
+            try:
+                for i in range(sd.shape[0]):
+                    year: int = sd["Year"].iloc[i]
+                    event: str = sd["Event"].iloc[i]
+                    location: str = sd["Location"].iloc[i]
+                    # source_date = sd["Source Date"].iloc[i]
+                    source: str = sd["Source"].iloc[i]
+                    page: int = sd["Page"].iloc[i]
+                    notes: str = sd["Notes"].iloc[i]
+
+                    if isinstance(year, str):
+                        year = np.nan
+
+                    rows.append(
+                        dict(
+                            county=_cleanup(county_name),
+                            fair=_cleanup(fair),
+                            year=year,
+                            event=_cleanup(event),
+                            location=_cleanup(location),
+                            source=_cleanup(source),
+                            page=page,
+                            notes=_cleanup(notes),
+                        )
+                    )
+            except Exception as e:
+                print(county_name, fair)
+
+                print(f"ERROR: {e}")
+
+                print(f"columns: {list(sd.columns)}")
+
+    reformated_data = pd.DataFrame.from_records(rows).drop_duplicates()
+    # reformated_data["source_date"] = reformated_data["source_date"].apply(
+    #     lambda x: pd.to_datetime(x).strftime("%d/%m/%Y")
+    # )
+    # reformated_data["source_date"] = reformated_data["source_date"].dt.date
+
+    # def _convert_if_timestamp(value):
+    #     if isinstance(value, pd.Timestamp):
+    #         return value.to_pydatetime()
+    #     return value
+
+    # reformated_data["source_date"] = reformated_data["source_date"].apply(
+    #     _convert_if_timestamp
+    # )
+    # reformated_data["source_date"] = reformated_data["source_date"].dt.date
+    # reformated_data["type_column"] = reformated_data["source_date"].apply(type)
+    reformated_data.to_csv("csv/reformated_sources.csv", index=False)
+    print("data")
+
+
 def add_apple_totals(df):
-    a = df.groupby(["county", "fair", "year"]).count().reset_index()
+    a = df.groupby(["county", "fair", "year"])
+    a["given_var"].count().reset_index()
     b = a.rename(columns={"apple_type": "apple_totals"})
     c = b.drop(columns="county")
-    return pd.merge(df, c, on=["fair", "fair", "year", "year"], how="left")
+    return pd.merge(
+        df,
+        c,
+        on=["fair", "year", "given_variety", "alt_variety", "presumed_variety"],
+        how="left",
+    )
 
 
 def save_data(data):
-    data.to_csv("reformated_data.csv", index=False)
-    data.to_excel("reformated_exel.xlsx")
-
-
-def geo_add():
-    df = pd.read_csv(
-        "/Users/jwilliamson/git/plasma_particle_simulator/Apple-mapping/reformated_data.csv"
-    )
-    cd = geopd.read_file(
-        "https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/US-counties.geojson"
-    )
-    df.groupby()
-    mc = cd[cd["STATE"] == "23"].reset_index()
-    mc = mc.drop(
-        columns=["STATE", "index", "id", "GEO_ID", "CENSUSAREA", "COUNTY", "LSAD"]
-    )
-    mc = mc.rename(columns={"NAME": "county"})
-    print(mc)
-
-    g = mc.merge(df, on=["county", "county"], how="right")
-    print(g)
+    data.to_csv("csv/reformated_data.csv", index=False)
+    print("done writing files")
 
 
 def main():
-
-    geo_add()
-    print(4)
-    # df = get_date_from_excel()
-    # df = add_apple_totals(df)
-    # save_data(df)
-    # print("done")
+    # df = get_data_from_source()
+    rsc = pd.read_csv("csv/reformated_sources_clean.csv")
+    tc = pd.read_csv(
+        "/Users/jwilliamson/git/plasma_particle_simulator/Apple-mapping/codes/town cords.csv",
+        sep="\t",
+    )
+    rs = rsc["Location"]
+    tb = tc["Location"]
+    g = rs.merge(tb, on="Location", how="left")
+    print("all done")
 
 
 if __name__ == "__main__":
